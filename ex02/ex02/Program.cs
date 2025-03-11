d7b03793ed281d93398265eab525677311a1652b
Anthony Dionisio dos Santos <anthony_dsant@outlook.com>
2025-03-10 23:01:43 -0300

    ex02

---
+++ ex02/ex02/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// Número informado
+int num = 1597;
+
+int num1 = 0;
+int num2 = 1;
+int temp = 0;
+
+Console.WriteLine($"Número procurado: {num}\n");
+
+while (num1 <= num)
+{
+    num1 = temp + num2;
+    temp = num2;
+    num2 = num1;
+
+    Console.WriteLine(num1);
+    
+    if (num1 == num)
+    {
+        Console.WriteLine("O número se encontra na sequência de Fibonnaci!");
+        break;
+    }
+}
+
+if (num1 != num)
+    Console.WriteLine("O número não se encontra na sequência de Fibonnaci!");
