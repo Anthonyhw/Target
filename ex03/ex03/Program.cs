917739e4db7f55d4dc2d9857f7c58bdfc0e1526d
Anthony Dionisio dos Santos <anthony_dsant@outlook.com>
2025-03-10 23:22:40 -0300

    ex03

---
+++ ex03/ex03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+using (StreamReader r = new StreamReader("Data/dados.json"))
+{
+    var json = r.ReadToEnd();
+    var items = JsonSerializer.Deserialize<List<Faturamento>>(json);
+
+    items!.RemoveAll(i => i.valor == 0);
+
+    var media = items.Average(i => i.valor);
+
+    Console.WriteLine($"Menor valor de faturamento ocorrido em um dia do mês: {items.Min(i => i.valor)}");
+
+    Console.WriteLine($"Maior valor de faturamento ocorrido em um dia do mês: {items.Max(i => i.valor)}");
+    
+    Console.WriteLine($"Número de dias no mês em que o valor de faturamento diário foi superior à média mensal. {items.Where(i => i.valor > media).Count()}");
+}
+
+public class Faturamento
+{
+    public int dia { get; set; }
+    public decimal valor { get; set; }
+}
