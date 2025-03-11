f214418951d97b506357ce7493dbd80435a28b45
Anthony Dionisio dos Santos <anthony_dsant@outlook.com>
2025-03-10 23:49:50 -0300

    ex05

---
+++ ex05/ex05/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Inserção de string
+string input = "Inverta esta string!";
+
+Console.WriteLine($"String original: {input}");
+
+
+char[] chars = new char[input.Length];
+
+for (int i = chars.Length - 1; i >= 0; i--)
+{
+    for (int j = chars.Length; j >= chars.Length ;j--)
+    {
+        chars[(j - 1) - i] = input[i];
+    }
+}
+
+
+string output = new string(chars);
+
+Console.WriteLine($"String invertida: {output}");
