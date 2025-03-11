e2cf9dc3f1a7e9a5684a034b73dd0d565ca97317
Anthony Dionisio dos Santos <anthony_dsant@outlook.com>
2025-03-10 23:31:50 -0300

    ex04

---
+++ ex04/Program.cs
@@ -0,0 +1,16 @@
+﻿Dictionary<string, double> estados = new Dictionary<string, double>();
+
+estados.Add("SP", 67836.43);
+estados.Add("RJ", 36678.66);
+estados.Add("MG", 29229.88);
+estados.Add("ES", 27165.48);
+estados.Add("Outros", 19849.53);
+
+var total = estados.Sum(e => e.Value);
+Console.WriteLine($"Total: {total}");
+
+foreach (var estado in estados)
+{
+    Console.WriteLine($"percentual de representação do estado {estado.Key}: {Math.Round((estado.Value/total) * 100, 2)}%");
+}
+
